d47b767241b73de1ebb836b80f507d7434fff224
haneen-ontech <haneen.almusowi@ontariotechu.net>
2026-03-22 01:32:03 -0400

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ from Crypto.Util.number import inverse, bytes_to_long, long_to_bytes
 p_hex = "00FEC3ECFB4C5D564181F77A0C66628A5D"
 q_hex = "009BF26B2C78615AA375BDF190F92F5575"
 e_hex = "010001"
-c_hex = "87DFFC0C992C7B499BE35AFD790984FE4F7AF520FD06343DA0D3155F8179E057"
+c_hex = "99E740F55C1EBA349A062371930EDA76CA43A9974FC8825E879171A2E5F6D8CF"
 
 # Convert to integer
 p = int(p_hex, 16)
@@ -23,7 +23,7 @@ d = inverse(e, phi)
 
 print("d =", d)
 
-#plaintext = b"r4D_r5a"  # your desired message as bytes
+#plaintext = b"r5a_1s_rad"  # your desired message as bytes
 #m = bytes_to_long(plaintext)        # convert to integer
 #print("m as integer:", m)
 
